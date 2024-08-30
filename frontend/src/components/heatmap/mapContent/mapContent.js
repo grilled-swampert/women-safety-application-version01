80af63aa6722ed73f63607280b6ff3f7089fe2e9
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2024-08-30 22:59:12 +0530

    heatmap base code

---
+++ frontend/src/components/heatmap/mapContent/mapContent.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./mapContent.css";
+
+import cctvFootage from "../photos/cctvFootage.svg";
+import sampleMap from "../photos/sampleMap.png";
+
+export default function MapContent() {
+  return (
+    <div className="container">
+      <h2>Map:</h2>
+      <div className="map-container">
+        <img src={sampleMap} alt="Map" className="map-image" />
+        <div className="alert-overlay">
+          <span className="alert-icon">!</span>
+          ALERT (1)
+        </div>
+      </div>
+      <div className="cctv-container">
+        <div className="cctv-information">
+          <div className="cctv-image">
+            <img src={cctvFootage} alt="CCTV Footage" />
+          </div>
+          <div className="cctv-info">
+            <h3>CCTV Footage</h3>
+            <p>
+              <strong>KJ Somaiya College of Engineering</strong>
+            </p>
+            <p className="cctv-imp-info">
+              Body text for your whole article or post. We'll put in some lorem
+              ipsum to show how a filled-out page might look:
+            </p>
+            <p>
+              Excepteur efficient emerging, minim veniam anim aute carefully
+              curated Ginza conversation exquisite perfect nostrud nisi
+              intricate Content. Qui international first-className nulla ut.
+              Punctual adipisicing, essential lovely queen tempor eiusmod irure.
+              Exclusive bespoke charming Scandinavian impeccable aute quality of
+              life soft power pariatur Melbourne occaecat discerning. Qui
+              wardrobe aliquid, et Porter destination Toto remarkable officia
+              Helsinki excepteur Basset hound. Zürich sleepy perfect
+              consectetur.
+            </p>
+          </div>
+        </div>
+        <div className="button-container">
+          <button className="button button-primary">
+            Send Backup to Location
+          </button>
+          <button className="button button-secondary">
+            Send Alert to Nearest Police Station
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/heatmap/heatmapPage.js
+++ frontend/src/pages/heatmap/heatmapPage.js
@@ -1,11 +1,11 @@
+import MapContent from "../../components/heatmap/mapContent/mapContent";
 import Navbar from "../../components/heatmap/navbar/navbar";
-
-
 
 export default function HeatmapPage() {
     return (
         <div>
             <Navbar />
+            <MapContent />
         </div>
     );
 }
