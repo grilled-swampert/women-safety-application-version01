f6b1594a4f1442baab1d7a6980024371e310539f
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2024-08-30 19:33:06 +0530

    added login parts

--- frontend/src/components/loginPage/loginForm/loginForm.js
+++ frontend/src/components/loginPage/loginForm/loginForm.js
@@ -1,7 +1,30 @@
+import "./loginForm.css";
+
 export default function LoginForm() {
-    return (
-        <div>
-            <h1>Login Form</h1>
+  return (
+    <div className="login-wrapper">
+      <div className="login-card">
+        <div className="login-card-inner">
+          <div className="login-card-front">
+            <div className="login-title">Login</div>
+            <form className="login-form" action="">
+              <input
+                className="login-input"
+                name="email"
+                placeholder="Enter your email"
+                type="email"
+              />
+              <input
+                className="login-input"
+                name="password"
+                placeholder="Enter your password"
+                type="password"
+              />
+              <button className="login-button">Let's go!</button>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,16 +1,36 @@
-export default function Navbar() {
+import React from 'react';
+import Menu from '../navbar/photos/menu.svg';
+import './navbar.css';
+
+const Navbar = () => {
     return (
-        <div className="navbar-container">
-            <nav>
-                <div className="nav-logo">
-                    <h1>Logo</h1>
-                </div>
-                <ul> 
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Contact</a></li>
-                </ul>
-            </nav>
+        <div>
+            <header>
+                <nav className="navbar-container">
+                    <div className="navbar-logo">
+                        <h4 data-aos="fade-down">Rembrandt</h4>
+                        <div className="navbar-logo-overlay"></div>
+                    </div>
+
+                    <ul className="navbar-menu" data-aos="fade-down">
+                        <li><a href="#visit">Visit</a></li>
+                        <li><a href="#yourbooks">Your Books</a></li>
+                        <li><a href="#catalogue">Catalogue</a></li>
+                        <li><a href="#about-us">About Us</a></li>
+                        <li>
+                            <button className="navbar-login-button">Login</button>
+                        </li>
+                    </ul>
+
+                    <ul className="navbar-mobile" data-aos="fade-down">
+                        <li>
+                            <img src={Menu} alt="Menu" />
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         </div>
-    )
+    );
 }
+
+export default Navbar;
