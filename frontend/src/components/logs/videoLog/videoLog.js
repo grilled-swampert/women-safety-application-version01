fcef50fe1e3ccbea5f0944ea455e52f6cb1c9adb
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2024-08-31 13:57:56 +0530

    logs layout

--- frontend/src/components/logs/videoLog/videoLog.js
+++ frontend/src/components/logs/videoLog/videoLog.js
@@ -3,20 +3,23 @@ import { Search } from 'lucide-react';
 import './videoLog.css';
 
 const videoData = [
-  { id: 1, location: 'Ghatkopar West', date: '24/05/2024', time: '9:40 PM', approxMen: 4 },
-  { id: 2, location: 'Vidyavihar West', date: '24/05/2024', time: '9:40 PM', approxMen: 4 },
-  { id: 3, location: 'Kurla West', date: '24/05/2024', time: '9:40 PM', approxMen: 4 },
-  { id: 4, location: 'Mulund West', date: '24/05/2024', time: '9:40 PM', approxMen: 4 },
+  { id: 1, location: 'KJ SOMAIYA', area: 'Ghatkopar West', date: '24/05/2024', time: '9:40 PM', approxMen: 4 },
+  { id: 2, location: 'Vidyavihar West', area: 'Ghatkopar West', date: '24/05/2024', time: '9:40 PM', approxMen: 4 },
+  { id: 3, location: 'Kurla West', area: 'Ghatkopar West', date: '24/05/2024', time: '9:40 PM', approxMen: 4 },
+  { id: 4, location: 'Mulund West', area: 'Ghatkopar West', date: '24/05/2024', time: '9:40 PM', approxMen: 4 },
 ];
 
 const VideoCard = ({ video }) => (
   <div className="video-card">
     <div className="video-placeholder"></div>
-    <p><strong>Location:</strong> {video.location}</p>
-    <p><strong>Date:</strong> {video.date}</p>
-    <p><strong>Time:</strong> {video.time}</p>
-    <p><strong>Approx No. of Men:</strong> {video.approxMen}</p>
-    <button className="more-info-btn">More Info</button>
+    <div className='video-info'>
+      <p className='location'>{video.location}</p>
+      <p><strong>Area:</strong> {video.area}</p>
+      <p><strong>Date:</strong> {video.date}</p>
+      <p><strong>Time:</strong> {video.time}</p>
+      <p><strong>Approx No. of Men:</strong> {video.approxMen}</p>
+      <button className="more-info-btn">View More</button>
+    </div>
   </div>
 );
 
