a9bf06c1069486b7f5346d75f583529354d06568
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2024-09-01 18:11:28 +0530

    log images

--- frontend/src/components/logs/videoLog/videoLog.js
+++ frontend/src/components/logs/videoLog/videoLog.js
@@ -2,16 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { Search } from 'lucide-react';
 import './videoLog.css';
 
+import CCTV1 from '../photos/cctv1.png';
+import CCTV2 from '../photos/cctv2.png';
+import CCTV3 from '../photos/cctv3.png';
+import CCTV4 from '../photos/cctv4.png';
+
 const videoData = [
-  { id: 1, location: 'KJ Somaiya College of Engineering', area: 'Vidyavihar West', approxMen: 6 },
-  { id: 2, location: 'Senroofs', area: 'Mulund East', approxMen: 1 },
-  { id: 3, location: 'St. Xaviers High School', area: 'Thane West', approxMen: 5 },
-  { id: 4, location: 'Pheonix Paragon Plaza', area: 'Kurla West', approxMen: 2 },
+  { id: 1, location: 'KJ Somaiya College of Engineering', area: 'Vidyavihar West', approxMen: 6, photo: <img src={CCTV1} alt="CCTV1" /> },
+  { id: 2, location: 'Senroofs', area: 'Mulund East', approxMen: 1, photo: <img src={CCTV2} alt="CCTV2" /> },
+  { id: 3, location: 'St. Xaviers High School', area: 'Thane West', approxMen: 5, photo: <img src={CCTV3} alt="CCTV3" /> },
+  { id: 4, location: 'Pheonix Paragon Plaza', area: 'Kurla West', approxMen: 2, photo: <img src={CCTV4} alt="CCTV4" /> },
 ];
 
 const VideoCard = ({ video }) => (
   <div className="video-card">
-    <div className="video-placeholder"></div>
+    <div className="video-placeholder">
+      {video.photo}
+    </div>
     <div className='video-info'>
       <p className='location'>{video.location}</p>
       <p><strong>Area:</strong> {video.area}</p>
